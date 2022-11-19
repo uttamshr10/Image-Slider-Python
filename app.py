7528294cc06891439f7cac7929a341ac537bd1c1
Uttam <utam.shrestha65@gmail.com>
2022-11-19 21:08:42 +0545

    App class added

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("hello world")
+from itertools import cycle
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self, image_files, delay):
+        tk.Tk.__init__(self):
+        self.geometry('+ {}+ {}'format(x, y))
+        self.delay = delay
+        self.pictures = cycle((tk.PhotoImage(file=image), image)
+                              for image in image_files)
+        self.pictures_display = tk.Label(self)
+        self.pictures_display.pack()
